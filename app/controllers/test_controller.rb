02ed62c8f40eb5e5118d0e8c5690c4edd021bab7
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2016-04-03 22:47:43 +0100

    Tests acks

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -5,10 +5,12 @@ class TestController < ApplicationController
     action = params["action"]
     if resource_type == 'author' && action == 'create'
       puts "About to create the Author: #{params['resource']}"
-      actor = Author.new(name: params['resource']['name'])
+      name = params['resource']['name']
+      actor = Author.new(name: name)
       actor.save!
       # need to write an ack message to "test_ack" topic
-      message = WaterDrop::Message.new("test_ack", {message: "author created"}.to_json)
+      sleep(5)
+      message = WaterDrop::Message.new("test_ack", {message: "author #{name} created"}.to_json)
       message.send!
     end
   end
